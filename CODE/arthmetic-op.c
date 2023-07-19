4c445cadb5b1e9e1d53af89c4feda6e9a9498104
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2023-07-19 10:08:10 +0530

    added new changess

--- CODE/arthmetic-op.c
+++ CODE/arthmetic-op.c
@@ -19,11 +19,54 @@ int main()
     // printf("result %d", 1%0);
 
 
-    int a = 10;
-    ++a;
-    a++;
-    a--;
-    printf("a = %d" , ++a);
+    // int a = 10;
+    // ++a;
+    // a++;
+    // a--;
+    // printf("a = %d" , ++a);
+
+    // relational operators
+
+    // printf("%d\n", 'a' > 'A');
+    // printf("%d\n", '0' > '9');
+    // printf("%d\n", 10.5 > 2.5);
+
+
+    //logical operators 
+
+    // int x = 5;
+    // int y = 2;
+    // int and_result = x < 5 && y <10;
+    // int or_result = x < 5 || y > 10;
+    // int exor_result =  x ^ 2 ;
+
+    // printf("%d %d %d" , and_result, or_result , exor_result);
+
+    // int x = 2;
+    // int y = 7;
+
+    // printf("%d\n" , (x & y));  // & -- bitwise and 
+    // printf("%d\n" , (x | y));
+    // printf("%d\n" , (x ^ y));
+
+    // int d = 100;
+    // printf("%d\n" , ( d << 2));
+    // printf("%d\n" , (d >> 1));
+
+    // int  age  =  16;
+
+	// (age>=18)? (printf("eligible for voting")) : (printf("not eligible for voting"));
+
+    // int num = -2;
+    // num > 0 ?  num%2 == 0 ? printf("even") : printf("odd") : printf("negative..");
+
+     int x, a=4, b=2;
+    x = a * a - 3 * b + a / b;
+
+    printf("x = %d\n", x);
+
+
+    
 
 
     
